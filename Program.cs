d4913699393b6ada5cacaaf8d2423d39b6f9a6d3
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-13 18:48:20 +0530

    Calculation of monthly Wage

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UC5Problem
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+        Demo demo = new Demo();
+        demo.Attendence();
+        
+        }
+    }
+}
+        
+
+
---
+++ UC5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC5Problem
+{
+    public class Demo
+    {
+        public  void Attendence()
+        {
+
+            int WagePerHour = 20;
+            int WorkingHour = 0;
+            int WagePerDay;
+            int WorkingDaysPerMonth = 20;
+            int WagePerMonth;
+
+
+            Random random = new Random();
+            int EmployeeCheck = random.Next(3);
+
+            {
+                if (EmployeeCheck == 1)
+
+                {
+                    Console.WriteLine("Employee Pesent for Full Time" );
+                    WorkingHour = 8;
+                }
+
+                else if (EmployeeCheck == 2)
+                {
+                    Console.WriteLine("Employee Pesent for Part Time");
+                    WorkingHour = 4;
+                }
+                else
+
+                {
+                    Console.WriteLine("Employee is Absent");
+                }
+            }
+            WagePerDay = WagePerHour * WorkingHour;
+            WagePerMonth = WagePerDay * WorkingDaysPerMonth;
+            Console.WriteLine("The Employee Wage for the Month is {0}", WagePerMonth);
+
+        }
+    }
+}
