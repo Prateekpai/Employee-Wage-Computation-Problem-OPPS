3a90051c4fa42537358fda389b43e76c2ef790b5
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-13 18:52:01 +0530

    Calculation of Wages till a condition of Total working hour or days reached for a month

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EmployeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Employee employee = new Employee();
+            employee.Attendence();
+        }
+    }
+}
---
+++ UC6.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+
+{
+    public class Employee
+    {
+        public  void Attendence()
+        {
+            int EmployeePresent = 1;
+            int EmployeeWagePerHour = 20;
+
+            int EmpHour = 0;
+            int EmpWage = 0;
+
+            int totalEmpHour = 0;
+            int totalEmpWorkingDays = 0;
+
+            int TotalWorkingHour = 100;
+            int TotalWorkingDays = 20;
+
+            while (totalEmpHour < TotalWorkingHour || totalEmpWorkingDays < TotalWorkingDays)
+            {
+                totalEmpWorkingDays++;
+                Random random = new Random();
+                int EmployeeCheck = random.Next(2);
+
+                if (EmployeeCheck == EmployeePresent)
+
+                {
+                    Console.WriteLine("Employee is Present");
+                    EmpHour = 8;
+                }
+
+                else
+
+                {
+                    Console.WriteLine("Employee is Absent");
+                    EmpHour = 0;
+                }
+                totalEmpHour = totalEmpHour + EmpHour;
+                {
+                    Console.WriteLine("Total Employee Working Hour is :" + totalEmpHour);
+                }
+            }
+        }
+    }
+}
