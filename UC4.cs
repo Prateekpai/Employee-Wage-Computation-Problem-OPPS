f6bd0504df5812a9c5d2c356b702c286a137ecc9
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-13 18:46:55 +0530

    Employee Wage Using Switch case

---
+++ UC4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EmployeeWageProblem
+{
+    public class Employee
+    {
+        public  void EmployeeCheck()
+        {
+            int EmployeeAbsent = 0;
+            int EmployeePresentFullTime = 1;
+            int EmployeePresentPartTime = 2;
+            int EmployeeWagePerHour = 20;
+            int WorkingHour = 0;
+            int WageFullTime;
+
+            Random random = new Random();
+            int EmployeeCheck = random.Next(3);
+
+
+            switch (EmployeeCheck)
+
+            {
+                case 0:
+                    Console.WriteLine("Employee Wage is Absent");
+                    WorkingHour = 0;
+                    break;
+
+                case 1:
+                    Console.WriteLine("Employee is Present For Full Day " );
+                    WorkingHour = 8;
+                    break;
+
+                case 2:
+                    Console.WriteLine("Employee is Present For Part Time " );
+                    WorkingHour = 4;
+                    break;    
+            }
+            WageFullTime = EmployeeWagePerHour * WorkingHour;
+            Console.WriteLine("Employee Wage is {0}", WageFullTime);
+        }
+    }
+}
