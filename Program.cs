63bcb00d3eb8f45d012da80a1b0225cb300842fd
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-13 18:37:36 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmployeeWage
+{
+    public class Employee
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
