1801adf2686a0db59c3b2510d3d35d1070174409
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-13 18:41:26 +0530

    Empployee Check Present or Absent

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeePresent employeePresent = new EmployeePresent();
+            employeePresent.Attendence();
+        }
+    }
+}
---
+++ UC1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApplication
+{
+    public class EmployeePresent
+    {
+        public void Attendence()
+        {
+            Random random = new Random();
+            int EmpCheck = random.Next(3);
+
+            if (EmpCheck == 1)
+                Console.WriteLine("Employee is present");
+            else if (EmpCheck == 2)
+                Console.WriteLine("Employee is present for Part Time");
+            else
+                Console.WriteLine("Employee is absent");
+
+        }
+    }
+}
