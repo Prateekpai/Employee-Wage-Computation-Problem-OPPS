c173740b04723cb5994c1cb2be00de407cf6608a
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-13 18:43:57 +0530

    Daily Wage Calculation

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace EmployeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            EmployeePresent employeePresent = new EmployeePresent();
+            employeePresent.Attendence();
+        }
+    }
+}
+    
+
---
+++ UC2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+
+{
+        public class EmployeePresent
+        {
+            public void Attendence()
+            {
+                Random random = new Random();
+                int EmpCheck = random.Next(2);
+                int WagePerHour = 20;
+                int WorkingHour = 0;
+                int EmployeeWage;
+
+                if (EmpCheck == 1)
+                {
+                Console.WriteLine("Employee is Present");
+                WorkingHour = 8;
+                }
+
+                else
+                    Console.WriteLine("Employee is absent ");
+                EmployeeWage = WagePerHour * WorkingHour;
+                Console.WriteLine("Employee Wage is {0}", EmployeeWage);
+
+            }
+        }
+}
